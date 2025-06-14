805493d521f5a3a69ebca5e68d894d693db384d1
TheVanadium <53013571+TheVanadium@users.noreply.github.com>
2025-06-14 14:53:50 -0700

    chore: remove unnecessary console.log

--- amiga-app/ts/src/pages/TrackSelect.tsx
+++ amiga-app/ts/src/pages/TrackSelect.tsx
@@ -59,8 +59,6 @@ export default function TrackSelect() {
         if (!tracksUpdate) {return;}
         const trackListEndpoint = `${import.meta.env.VITE_API_URL}/list_tracks`;
         const lineListEndpoint = `${import.meta.env.VITE_API_URL}/line/list`;
-        // hi
-        console.log("hi");
         
         fetch(trackListEndpoint, { method: "GET" })
         .then((response) => response.json())
