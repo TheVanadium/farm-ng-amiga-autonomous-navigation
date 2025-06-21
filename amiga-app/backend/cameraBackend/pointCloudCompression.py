b15aec93801e37d335f52808aa8fcfefffafe521
TheVanadium <53013571+TheVanadium@users.noreply.github.com>
2025-06-21 15:08:12 -0700

    doc: docstring compression

--- amiga-app/backend/cameraBackend/pointCloudCompression.py
+++ amiga-app/backend/cameraBackend/pointCloudCompression.py
@@ -3,12 +3,32 @@ import numpy as np
 import DracoPy
 
 def compress_pcd(pcd: o3d.geometry.PointCloud) -> bytes:
+    """Compresses an open3d PointCloud with Draco
+    
+    Args:
+        pcd (o3d.geometry.PointCloud): The point cloud to compress.
+        
+    Returns:
+        bytes: The compressed point cloud in Draco format.
+    """
+
     points = np.asarray(pcd.points, dtype=np.float32)
     colors = (np.asarray(pcd.colors) * 255).astype(np.uint8)
     return DracoPy.encode(points, colors=colors)
 
 
 def decompress_drc(draco_binary: bytes) -> o3d.geometry.PointCloud:
+    """Decompresses a Draco binary to an open3d PointCloud
+    Args:
+        draco_binary (bytes): The compressed point cloud in Draco format.
+        
+    Returns:
+        o3d.geometry.PointCloud: The decompressed point cloud.
+    
+    Raises:
+        ValueError: If the binary does not contain points or colors when
+            decoded with DracoPy.
+    """
     decoded_drc = DracoPy.decode(draco_binary)
     if not hasattr(decoded_drc, 'points'):
         raise ValueError("Input missing points")
