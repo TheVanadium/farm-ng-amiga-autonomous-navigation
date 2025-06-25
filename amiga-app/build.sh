471216be3df4ed83a70b5b7a32bb90f22103275c
TheVanadium <53013571+TheVanadium@users.noreply.github.com>
2025-06-25 12:59:47 -0700

    Update build.sh

--- amiga-app/build.sh
+++ amiga-app/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -x
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
 # create virtual environment for the backend
 python3 -m venv venv
 source venv/bin/activate
@@ -15,6 +12,8 @@ cd ts/
 sudo apt update
 sudo apt install curl
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install --lts
 npm install
 npm run build
