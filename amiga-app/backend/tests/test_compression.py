c5af919545147b0b65ff963a2cd49e160b2f2c52
TheVanadium <53013571+TheVanadium@users.noreply.github.com>
2025-08-06 14:20:33 -0700

    blacken

--- amiga-app/backend/tests/test_compression.py
+++ amiga-app/backend/tests/test_compression.py
@@ -2,6 +2,7 @@ from cameraBackend.pointCloudCompression import compress_pcd, decompress_drc
 import open3d
 import unittest
 
+
 class TestCompression(unittest.TestCase):
     def test_loss_suite(self) -> None:
         def _test_loss(filename: str) -> None:
@@ -10,8 +11,10 @@ class TestCompression(unittest.TestCase):
             pcd = pcd.voxel_down_sample(voxel_size=0.1)
             pcd2 = pcd2.voxel_down_sample(voxel_size=0.1)
             assert abs(len(pcd.points) - len(pcd2.points)) <= 0.001 * len(pcd.points)
+
         _test_loss("tests/test_data/test_1.ply")
         _test_loss("tests/test_data/test_2.ply")
+
 
 if __name__ == "__main__":
     unittest.main()
