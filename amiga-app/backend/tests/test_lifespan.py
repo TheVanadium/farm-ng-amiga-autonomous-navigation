15ef21592c24988e37f1943e25ce98094d24c69f
TheVanadium <53013571+TheVanadium@users.noreply.github.com>
2025-08-07 13:59:39 -0700

    fix test data import location

--- amiga-app/backend/tests/test_lifespan.py
+++ amiga-app/backend/tests/test_lifespan.py
@@ -1,6 +1,7 @@
 import unittest
 from main import setup_services, Arguments
 from multiprocessing import Queue
+import os
 
 # this is bad, but I can't figure out how to import config mypy complaining
 import config  # type: ignore
@@ -8,7 +9,8 @@ import config  # type: ignore
 
 class TestLifespan(unittest.IsolatedAsyncioTestCase):
     async def test_setup_services(self):
-        args = Arguments(config="config.json", port=config.PORT)
+        config_path = os.path.join(os.path.dirname(__file__), "test_data", "config.json")
+        args = Arguments(config=config_path, port=config.PORT)
         for service in await setup_services(args, Queue()):
             self.assertTrue(service is not None)
 
