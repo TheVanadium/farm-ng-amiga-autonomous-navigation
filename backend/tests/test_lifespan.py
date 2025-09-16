8926fd8b0ba179876946c06a65b34c152903a40f
TheVanadium <53013571+TheVanadium@users.noreply.github.com>
2025-09-16 15:15:13 -0700

    fix: backend test Argument import

--- backend/tests/test_lifespan.py
+++ backend/tests/test_lifespan.py
@@ -1,7 +1,8 @@
 import unittest
-from main import setup_services, Arguments
+from main import setup_services
 from multiprocessing import Queue
 import os
+from argparse import Namespace
 import config
 
 
@@ -10,7 +11,7 @@ class TestLifespan(unittest.IsolatedAsyncioTestCase):
         config_path = os.path.join(
             os.path.dirname(__file__), "test_data", "config.json"
         )
-        args = Arguments(config=config_path, port=config.PORT)
+        args = Namespace(config=config_path, port=config.PORT)
         for service in await setup_services(args, Queue()):
             self.assertTrue(service is not None)
 
